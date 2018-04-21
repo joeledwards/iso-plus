ec101507868a6d88cfa52b4fbb881ccf6fa377c2
Joel Edwards <joeledwards@gmail.com>
2018-04-21 00:22:58 -0600

    added time prefix caching to speed up time assemblies (generally twice as fast)

--- app/index.js
+++ app/index.js
@@ -10,7 +10,65 @@ clock.granularity = "seconds";
 
 // Heart Rate Sensor
 let hrm = new HeartRateSensor();
-let hrSummary = hrError();
+
+// Local Date
+let cachedLocalYear;
+let cachedLocalMonth;
+let cachedLocalDay;
+let lastLocalYear;
+let lastLocalMonth;
+let lastLocalDay;
+function localDate(date) {
+  let yearUpdated = false;
+  let monthUpdated = false;
+
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+
+  if (year !== lastLocalYear) {
+    yearUpdated = true;
+    lastLocalYear = year;
+    cachedLocalYear = monoDigits(year);
+  }
+  if (yearUpdated || month !== lastLocalMonth) {
+    monthUpdated = true;
+    lastLocalMonth = month;
+    cachedLocalMonth = `${cachedLocalYear}-${monoDigits(month)}`;
+  }
+  if (monthUpdated || day !== lastLocalDay) {
+    lastLocalDay = day;
+    cachedLocalDay = `${cachedLocalMonth}-${monoDigits(day)}`;
+  }
+
+  return cachedLocalDay;
+}
+
+// Local Time
+let cachedLocalHour;
+let cachedLocalMinute;
+let lastLocalHour;
+let lastLocalMinute;
+function localTime(date) {
+  let hourUpdated = false;
+
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+
+  if (hour !== lastLocalHour) {
+    hourUpdated = true;
+    lastLocalHour = hour;
+    cachedLocalHour = monoDigits(hour);
+  }
+
+  if (hourUpdated || minute != lastLocalMinute) {
+    lastLocalMinute = minute;
+    cachedLocalMinute = `${cachedLocalHour}:${monoDigits(minute)}`;
+  }
+
+  return `${cachedLocalMinute}:${monoDigits(second)}`;
+}
 
 // Get a handle on the <text> element
 let dateLabel = document.getElementById("dateLabel");
@@ -19,33 +77,65 @@ let utcLabel = document.getElementById("utcLabel");
 let unixLabel = document.getElementById("unixLabel");
 let hrLabel = document.getElementById("hrLabel");
 
+// UTC Time
+let cachedUtcYear;
+let cachedUtcMonth;
+let cachedUtcDay;
+let cachedUtcHour;
+let cachedUtcMinute;
+let lastUtcYear;
+let lastUtcMonth;
+let lastUtcDay;
+let lastUtcHour;
+let lastUtcMinute;
 function utcTime(date) {
-  let year = monoDigits(date.getUTCFullYear());
-  let month = monoDigits(date.getUTCMonth() + 1);
-  let day = monoDigits(date.getUTCDate());
-  let hour = monoDigits(date.getUTCHours());
-  let minute = monoDigits(date.getUTCMinutes());
-  let second = monoDigits(date.getUTCSeconds());
+  let yearUpdated = false;
+  let monthUpdated = false;
+  let dayUpdated = false;
+  let hourUpdated = false;
 
-  return `${year}-${month}-${day}T${hour}:${minute}:${second}Z`;
+  let year = date.getUTCFullYear();
+  let month = date.getUTCMonth() + 1;
+  let day = date.getUTCDate();
+  let hour = date.getUTCHours();
+  let minute = date.getUTCMinutes();
+  let second = date.getUTCSeconds();
+
+  if (year !== lastUtcYear) {
+    yearUpdated = true;
+    lastUtcYear = year;
+    cachedUtcYear = monoDigits(year);
+  }
+
+  if (yearUpdated || month !== lastUtcMonth) {
+    monthUpdated = true;
+    lastUtcMonth = month;
+    cachedUtcMonth = `${cachedUtcYear}-${monoDigits(month)}`;
+  }
+
+  if (monthUpdated || day !== lastUtcDay) {
+    dayUpdated = true;
+    lastUtcDay = day;
+    cachedUtcDay = `${cachedUtcMonth}-${monoDigits(day)}`;
+  }
+
+  if (dayUpdated || hour !== lastUtcHour) {
+    hourUpdated = true;
+    lastUtcHour = hour;
+    cachedUtcHour = `${cachedUtcDay}T${monoDigits(hour)}`;
+  }
+
+  if (hourUpdated || minute !== lastUtcMinute) {
+    lastUtcMinute = minute;
+    cachedUtcMinute = `${cachedUtcHour}:${monoDigits(minute)}`;
+  }
+
+  return `${cachedUtcMinute}:${monoDigits(second)}Z`;
 }
 
+// UNIX Time
 function unixTime(date) {
   return monoDigits((date.getTime() / 1000) | 0);
-}
-
-function localTime(date) {
-  let year = monoDigits(date.getFullYear());
-  let month = monoDigits(date.getMonth() + 1);
-  let day = monoDigits(date.getDate());
-  let hour = monoDigits(date.getHours());
-  let minute = monoDigits(date.getMinutes());
-  let second = monoDigits(date.getSeconds());
-  
-  return {
-    date: `${year}-${month}-${day}`,
-    time: `${hour}:${minute}:${second}`
-  };
 }
 
 // Update the <text> element with the current time
@@ -53,15 +143,14 @@ function updateClock(ts) {
   //let start = Date.now();
   
   let now = (ts instanceof Date) ? ts : new Date();
-  let {date, time} = localTime(now);
-  let utc = utcTime(now);
-  let unix = unixTime(now);
 
-  dateLabel.text = date;
-  timeLabel.text = time;
-  utcLabel.text = utc;
-  unixLabel.text = unix;
-  hrLabel.text = hrSummary;
+  dateLabel.text = localDate(now);
+  timeLabel.text = localTime(now);
+  utcLabel.text = utcTime(now);
+  unixLabel.text = unixTime(now);
+
+  // Update the heart rate
+  updateHeartRate();
   
   //unixLabel.text = `⏱ ${(Date.now() - start) / 1000} s`;
 }
@@ -69,30 +158,27 @@ function updateClock(ts) {
 // Update the clock every tick event
 clock.ontick = ({date}) => updateClock(date);
 
-function hrReport() {
-  lastHrUpdate = Date.now();
-  let heartRate = monoDigits(hrm.heartRate);
-  //let heartRate = hrm.heartRate;
-  return `❤️ ${heartRate} ❤️`;
-}
-
-function hrError() {
-  return `❤️ -- ❤️`;
-}
-
+const emptyHr = `❤️ -- ❤️`;
 let lastHrUpdate = 0;
-setInterval(() => {
-  if (Date.now() - lastHrUpdate > 5000) {
-    hrSummary = hrError();
+let lastHeartRate;
+let heartRate;
+
+function updateHeartRate() {
+  if (heartRate == null || Date.now() - lastHrUpdate > 5000) {
+    hrLabel.text = emptyHr;
+  } else if (lastHeartRate !== heartRate) {
+    lastHeartRate = heartRate;
+    hrLabel.text = `❤️ ${monoDigits(heartRate)} ❤️`;
   }
-}, );
+}
 
 // Store the Heart Rate reading; update on clock tick
 hrm.onreading = reading => {
-  hrSummary = hrReport();
+  lastHrUpdate = Date.now();
+  heartRate = hrm.heartRate;
 };
 hrm.onerror = error => {
-  hrSummary = hrError();
+  heartRate = null;
 };
 
 // Don't start with a blank screen
