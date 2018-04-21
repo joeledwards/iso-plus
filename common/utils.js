3f106a82ef2a5b13e697a9f905cf43529e6587e3
Joel Edwards <joeledwards@gmail.com>
2018-04-20 22:09:05 -0600

    improved performance of the monoDigits() function

--- common/utils.js
+++ common/utils.js
@@ -1,7 +1,7 @@
-// Convert to a string containing monospace digits
-export function monoDigits(num) {
-  if (num < 10) {
-    return String.fromCharCode(0x10) + monoDigit(num);
+// Convert to a string containing monospace digits (zero pads left if < 10)
+export function monoDigits(num, pad = true) {
+  if (pad && num < 10) {
+    return c0 + monoDigit(num);
   } else {
     let monoNum = ''
     while (num > 0) {
@@ -12,17 +12,28 @@ export function monoDigits(num) {
   }
 }
 
+const c0 = String.fromCharCode(0x10);
+const c1 = String.fromCharCode(0x11);
+const c2 = String.fromCharCode(0x12);
+const c3 = String.fromCharCode(0x13);
+const c4 = String.fromCharCode(0x14);
+const c5 = String.fromCharCode(0x15);
+const c6 = String.fromCharCode(0x16);
+const c7 = String.fromCharCode(0x17);
+const c8 = String.fromCharCode(0x18);
+const c9 = String.fromCharCode(0x19);
+
 function monoDigit(num) {
   switch (num) {
-    case 0: return String.fromCharCode(0x10);
-    case 1: return String.fromCharCode(0x11);
-    case 2: return String.fromCharCode(0x12);
-    case 3: return String.fromCharCode(0x13);
-    case 4: return String.fromCharCode(0x14);
-    case 5: return String.fromCharCode(0x15);
-    case 6: return String.fromCharCode(0x16);
-    case 7: return String.fromCharCode(0x17);
-    case 8: return String.fromCharCode(0x18);
-    case 9: return String.fromCharCode(0x19);
+    case 0: return c0;
+    case 1: return c1;
+    case 2: return c2;
+    case 3: return c3;
+    case 4: return c4;
+    case 5: return c5;
+    case 6: return c6;
+    case 7: return c7;
+    case 8: return c8;
+    case 9: return c9;
   }
 }
