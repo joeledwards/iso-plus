aa17f262de1307f5dfc94d21451bbb068ffdf38d
Joel Edwards <joeledwards@gmail.com>
2018-04-20 20:29:53 -0600

    just times and heart rate now (no more location)

--- app/index.js
+++ app/index.js
@@ -3,113 +3,100 @@ import document from "document";
 import { HeartRateSensor } from "heart-rate";
 import { geolocation } from "geolocation";
 
-import * as util from "../common/utils";
+import {monoDigits} from "../common/utils";
 
 // Update the clock every minute
 clock.granularity = "seconds";
 
 // Heart Rate Sensor
-var hrm = new HeartRateSensor();
+let hrm = new HeartRateSensor();
+let hrSummary = hrError();
 
 // Get a handle on the <text> element
 let dateLabel = document.getElementById("dateLabel");
 let timeLabel = document.getElementById("timeLabel");
-let geoLabel = document.getElementById("geoLabel");
-let coordLabel = document.getElementById("coordLabel");
+let utcLabel = document.getElementById("utcLabel");
+let unixLabel = document.getElementById("unixLabel");
 let hrLabel = document.getElementById("hrLabel");
 
-// Update the <text> element with the current time
-function updateClock() {
-  let now = new Date();
-  let year = util.monoDigits(now.getYear() + 1900);
-  let month = util.monoDigits(util.zeroPad(now.getMonth() + 1));
-  let day = util.monoDigits(util.zeroPad(now.getDay()));
-  let hour = util.monoDigits(util.zeroPad(now.getHours()));
-  let minute = util.monoDigits(util.zeroPad(now.getMinutes()));
-  let second = util.monoDigits(util.zeroPad(now.getSeconds()));
-  
-  let heartRate = 60;
+function utcTime(date) {
+  let year = monoDigits(date.getUTCFullYear());
+  let month = monoDigits(date.getUTCMonth() + 1);
+  let day = monoDigits(date.getUTCDate());
+  let hour = monoDigits(date.getUTCHours());
+  let minute = monoDigits(date.getUTCMinutes());
+  let second = monoDigits(date.getUTCSeconds());
 
-  dateLabel.text = `${year}-${month}-${day}`;
-  timeLabel.text = `${hour}:${minute}:${second}`;
+  return `${year}-${month}-${day}T${hour}:${minute}:${second}Z`;
+}
+
+function unixTime(date) {
+  return monoDigits((date.getTime() / 1000) | 0);
+}
+
+function localTime(date) {
+  let year = monoDigits(date.getFullYear());
+  let month = monoDigits(date.getMonth() + 1);
+  let day = monoDigits(date.getDate());
+  let hour = monoDigits(date.getHours());
+  let minute = monoDigits(date.getMinutes());
+  let second = monoDigits(date.getSeconds());
+  
+  return {
+    date: `${year}-${month}-${day}`,
+    time: `${hour}:${minute}:${second}`
+  };
+}
+
+// Update the <text> element with the current time
+function updateClock(ts) {
+  //let start = Date.now();
+  
+  let now = (ts instanceof Date) ? ts : new Date();
+  let {date, time} = localTime(now);
+  let utc = utcTime(now);
+  let unix = unixTime(now);
+
+  dateLabel.text = date;
+  timeLabel.text = time;
+  utcLabel.text = utc;
+  unixLabel.text = unix;
+  hrLabel.text = hrSummary;
+  
+  //unixLabel.text = `⏱ ${(Date.now() - start) / 1000} s`;
 }
 
 // Update the clock every tick event
-clock.ontick = () => updateClock();
+clock.ontick = ({date}) => updateClock(date);
 
-function geoReport(position) {
-  let {accuracy, altitude, heading, latitude, longitude} = position.coords
-  let headingEmoji = '⬆️';
-  if (accuracy < 1) {
-    headingEmoji = '⏺';
-  } else if (heading > 340 || heading < 20) {
-    headingEmoji = '⬆️';
-  } else if (heading > 290) {
-    headingEmoji = '↖️';
-  } else if (heading > 250) {
-    headingEmoji = '⬅️';
-  } else if (heading > 200) {
-    headingEmoji = '↙️';
-  } else if (heading > 160) {
-    headingEmoji = '⬇️';
-  } else if (heading > 110) {
-    headingEmoji = '↘️';
-  } else if (heading > 70) {
-    headingEmoji = '➡️';
-  } else {
-    headingEmoji = '↗️';
-  }
-  let geoInfo = `${headingEmoji} ${heading}°, ${altitude}m`;
-  let coordinates = `${latitude}, ${longitude}`;
-  //console.log(geoInfo);
-  //console.log(coordinates);
-  geoLabel.text = geoInfo;
-  coordLabel.text = coordinates;
-}
-
-function geoError(error) {
-  geoLabel.text = `⏹ --°, --m`;
-  coordLabel.text = `--, --`
-}
-
-function checkLocation() {
-  let start = Date.now();
-  geolocation.getCurrentPosition(
-    reading => {
-      geoReport(reading);
-      setTimeout(checkLocation, 1000);
-    },
-    error => {
-      geoError(error);
-      setTimeout(checkLocation, 1000);
-    }
-  )
-}
-
-function hrReport(reading) {
+function hrReport() {
   lastHrUpdate = Date.now();
-  let heartRate = util.monoDigits(hrm.heartRate);
-  hrLabel.text = `❤️ ${heartRate} ❤️`;
+  let heartRate = monoDigits(hrm.heartRate);
+  //let heartRate = hrm.heartRate;
+  return `❤️ ${heartRate} ❤️`;
 }
 
-function hrError(error) {
-  hrLabel.text = `❤️ -- ❤️`;
+function hrError() {
+  return `❤️ -- ❤️`;
 }
 
 let lastHrUpdate = 0;
 setInterval(() => {
   if (Date.now() - lastHrUpdate > 5000) {
-    hrError();
+    hrSummary = hrError();
   }
 }, );
 
-hrm.onreading = reading => hrReport(reading);
-hrm.onerror = error => hrError(error);
+// Store the Heart Rate reading; update on clock tick
+hrm.onreading = reading => {
+  hrSummary = hrReport();
+};
+hrm.onerror = error => {
+  hrSummary = hrError();
+};
 
 // Don't start with a blank screen
 updateClock();
-hrError();
-geoError();
-checkLocation();
 
+// Read heart rate
 hrm.start();
--- common/utils.js
+++ common/utils.js
@@ -1,28 +1,28 @@
-// Add zero in front of numbers < 10
-export function zeroPad(i) {
-  if (i < 10) {
-    i = "0" + i;
+// Convert to a string containing monospace digits
+export function monoDigits(num) {
+  if (num < 10) {
+    return String.fromCharCode(0x10) + monoDigit(num);
+  } else {
+    let monoNum = ''
+    while (num > 0) {
+      monoNum = monoDigit(num % 10) + monoNum
+      num = (num / 10) | 0
+    }
+    return monoNum
   }
-  return i;
 }
 
-// Convert a number to a special monospace number
-export function monoDigits(digits) {
-  var ret = "";
-  var str = digits.toString();
-  for (var index = 0; index < str.length; index++) {
-    var num = str.charAt(index);
-    ret = ret.concat(hex2a("0x1" + num));
+function monoDigit(num) {
+  switch (num) {
+    case 0: return String.fromCharCode(0x10);
+    case 1: return String.fromCharCode(0x11);
+    case 2: return String.fromCharCode(0x12);
+    case 3: return String.fromCharCode(0x13);
+    case 4: return String.fromCharCode(0x14);
+    case 5: return String.fromCharCode(0x15);
+    case 6: return String.fromCharCode(0x16);
+    case 7: return String.fromCharCode(0x17);
+    case 8: return String.fromCharCode(0x18);
+    case 9: return String.fromCharCode(0x19);
   }
-  return ret;
-}
-
-// Hex to string
-export function hex2a(hex) {
-  var str = '';
-  for (var index = 0; index < hex.length; index += 2) {
-    var val = parseInt(hex.substr(index, 2), 16);
-    if (val) str += String.fromCharCode(val);
-  }
-  return str.toString();
 }
